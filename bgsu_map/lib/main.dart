e69f6e3745ddb0e5e331eafc2ffa919a073e6a8c
Bryant Whitaker <brywhit@bgsu.edu>
2023-03-31 22:10:49 -0400

    Begin implementation with flutterfire, create map and have pins appear on map.

--- bgsu_map/lib/main.dart
+++ bgsu_map/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'src/locations.dart' as locations;
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +11,28 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+  
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(41.380540, -83.630176);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final mapItems = await locations.getEvents();
+    setState(() {
+      _markers.clear();
+      for (final building in mapItems.buildings) {
+        final marker = Marker(
+          markerId: MarkerId(building.name),
+          position: LatLng(building.lat, building.lng),
+          infoWindow: InfoWindow(
+            title: building.name,
+            snippet: building.address,
+          ),
+        );
+        _markers[building.name] = marker;
+      }
+    });
   }
 
   @override
@@ -28,15 +44,16 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Bowling Green Events'),
+          title: const Text('Bowling Green State University Buildings'),
           elevation: 2,
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(41.377925, -83.639979),
             zoom: 14,
           ),
+          markers: _markers.values.toSet(),
         ),
       ),
     );
