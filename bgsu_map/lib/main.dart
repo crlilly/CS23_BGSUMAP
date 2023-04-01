b6d160af0e839f6074a29a0e06fa5da6bdaabecd
Bryant Whitaker <brywhit@bgsu.edu>
2023-04-01 09:59:43 -0400

    Update main.dart to remove unnecessary code, remove data/ folder as it was unused

--- bgsu_map/lib/main.dart
+++ bgsu_map/lib/main.dart
@@ -1,19 +1,12 @@
 //import 'dart:html';
 
-import 'package:bgsu_map/data/services/service_locator.dart';
-import 'package:bgsu_map/webScraper.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'data/services/permission/permission_service.dart';
-import 'package:flutter/cupertino.dart';
-import 'data/services/service_locator.dart';
-import 'data/services/media/media_service_interface.dart';
 import 'src/locations.dart' as locations;
 
 void main() async {
-  setupServiceLocator();
   runApp(const MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -27,24 +20,16 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-final _permissionService = getIt<PermissionService>();
-BitmapDescriptor icon = BitmapDescriptor.defaultMarker;
-
 class _MyAppState extends State<MyApp> {
-  final Future<dynamic> _events = extractData();
   final Map<String, Marker> _markers = {};
   Future<void> _onMapCreated(GoogleMapController controller) async {
     final mapItems = await locations.getEvents();
     setState(() {
       _markers.clear();
       for (final building in mapItems.buildings) {
-        BitmapDescriptor.fromAssetImage(
-                const ImageConfiguration(), building.image)
-            .then((value) => icon = value);
         final marker = Marker(
             markerId: MarkerId(building.name),
             position: LatLng(building.lat, building.lng),
-            icon: icon,
             infoWindow: InfoWindow(
               title: building.name,
               snippet: building.address,
@@ -56,6 +41,8 @@ class _MyAppState extends State<MyApp> {
       }
     });
   }
+
+  @override
 
   @override
   Widget build(BuildContext context) {
