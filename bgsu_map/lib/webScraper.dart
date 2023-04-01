4ad3730b4265778d2c15b97457f01121e26b6cb0
Kayla Senczyszyn <ksenczy@bgsu.edu>
2023-03-31 23:42:41 -0400

    updated webScraper file

--- bgsu_map/lib/webScraper.dart
+++ bgsu_map/lib/webScraper.dart
@@ -1,20 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
+
 Future<List<String>> extractData() async {
-    //form connection to webpage
-    final response =
-        await http.Client().get(Uri.parse('https://www.geeksforgeeks.org/'));
+  //form connection to webpage
+  final response =
+      await http.Client().get(Uri.parse('https://www.geeksforgeeks.org/'));
 
-        if (response.statusCode == 200) { //if connection is formed
-            //get html; document from page
-            var document = parser.parse(response.body);
+  if (response.statusCode == 200) {
+    //if connection is formed
+    //get html; document from page
+    var document = parser.parse(response.body);
 
-            try {
-
-                
-            }  catch (e) {
-        return ['', '', 'ERROR!'];
-      }
-    } else {
-      return ['', '', 'ERROR: ${response.statusCode}.'];
+    try {} catch (e) {
+      return ['', '', 'ERROR!'];
     }
-
+  } else {
+    return ['', '', 'ERROR: ${response.statusCode}.'];
+  }
 }
